dae6744fb8c6d33e905d385e806f6e76293f532c
18120525 <thanhquang11220@gmail.com>
2023-03-29 10:04:37 +0700

    configure router/layout for project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { publicRoutes } from "~/routes";
+import DefaultLayout from "~/components/Layout/DefaultLayout";
 
 function App() {
-  return(
-    <div className="app">
-      <h1>React</h1>
-    </div>
-  )
+  return (
+    <Router>
+      <div className="app">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Page = route.component; //muốn dùng component vs JSX thi viết hoa chữ đầu
+
+            let Layout = DefaultLayout;
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
+
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
