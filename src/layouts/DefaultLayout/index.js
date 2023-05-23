60a50e5c992a04cdd9ddf5b0a833a12d956d5ea7
18120525 <thanhquang11220@gmail.com>
2023-05-23 08:39:10 +0700

    Code refactoring, optimization and use Proptypes

--- src/layouts/DefaultLayout/index.js
+++ src/layouts/DefaultLayout/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import Header from "../components/Header";
 import Sidebar from "./Sidebar";
 
@@ -17,4 +18,7 @@ function DefaultLayout({ children }) {
   );
 }
 
+DefaultLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
 export default DefaultLayout;
